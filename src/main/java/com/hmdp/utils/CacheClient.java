789b0813a5a2383dabd9804fd59f9ce7487c81f7
hjd <hjd@atguigu.com>
2026-04-16 11:36:42 +0800

    git commit -m "店铺数据查不到"

--- src/main/java/com/hmdp/utils/CacheClient.java
+++ src/main/java/com/hmdp/utils/CacheClient.java
@@ -1,7 +1,9 @@
 package com.hmdp.utils;
 
 import cn.hutool.cache.Cache;
+import cn.hutool.core.util.BooleanUtil;
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import com.hmdp.entity.Shop;
 import lombok.extern.slf4j.Slf4j;
@@ -9,6 +11,8 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 
@@ -34,7 +38,7 @@ public class CacheClient {
         redisData.setData(value);
         redisData.setExpireTime(LocalDateTime.now().plusSeconds(unit.toSeconds(time)));
         //写入redis
-        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(value), time, unit);
+        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(redisData));
     }
     public <R, ID> R queryWithPassThrough(String keyPrefix, ID id, Class<R> type, Function<ID, R> dbFallback,Long time, TimeUnit unit) {
         String key=keyPrefix+id;
@@ -59,5 +63,59 @@ public class CacheClient {
         this.set(key, r, time, unit);
         //7.返回
         return r;
+    }
+
+    private static final ExecutorService CACHE_REBUILD_EXECUTOR = Executors.newFixedThreadPool(10);
+
+    public <R,ID> R queryWithLogicalExpire(String keyPrefix,ID id,Class<R> type,Function<ID,R> dbFallback,Long time, TimeUnit unit) {
+        String key=keyPrefix+id;
+        //1.从Redis中查询缓存
+        String json = stringRedisTemplate.opsForValue().get(key);
+        //2.判断是否存在
+        if (StrUtil.isBlank(json)) {
+            //3.不存在，直接返回null（需要预先将数据写入Redis）
+            return null;
+        }
+        //4.命中，先把json反序列化
+        RedisData redisData = JSONUtil.toBean(json, RedisData.class);
+        R r= JSONUtil.toBean((JSONObject) redisData.getData(),type);
+        LocalDateTime expireTime = redisData.getExpireTime();
+        //5.判断是否过期
+        if (expireTime == null || expireTime.isAfter(LocalDateTime.now())) {
+            //5.1未过期，直接返回
+            return r;
+        }
+        //5.2已过期，需要缓存重建
+        //6.缓存重建
+        //6.1获取互斥锁
+        String lockKey = LOCK_SHOP_KEY + id;
+        boolean isLocked = tryLock(lockKey);
+        if (isLocked) {
+            //6.2获取互斥锁成功，进行缓存重建
+            CACHE_REBUILD_EXECUTOR.submit(() -> {
+                try {
+                    //重建缓存
+                    //查询数据库
+                    R r1 = dbFallback.apply(id);
+                    //写入redis
+                    this.setWithLogicalExpire(key, r1, time, unit);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    unlock(lockKey);
+                }
+            });
+        }
+        //7.返回旧数据
+        return r;
+    }
+
+    private boolean tryLock(String key){
+        Boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(key, "1", 10, TimeUnit.MINUTES);
+        return BooleanUtil.isTrue(flag);
+    }
+
+    private void unlock(String key){
+        stringRedisTemplate.delete(key);
     }
 }
